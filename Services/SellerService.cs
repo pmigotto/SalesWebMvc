8e78cc8feac050b5b63e4b8780101a20e693c60e
Pmigotto <pmigotto@hotmail.com>
2024-09-03 23:42:43 -0300

    SellerService and find all

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -1,4 +1,21 @@
-﻿namespace SalesWebMvc.Services {
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Services {
     public class SellerService {
+
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context) {
+
+            _context = context;
+        }
+
+        public List<Seller>FindAll(){
+            return _context.Seller.ToList();
+        }
+
     }
 }
