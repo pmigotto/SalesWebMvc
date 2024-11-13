7c8520325233bf57ed8134cee57876eaf9b7ce2c
Pmigotto <pmigotto@hotmail.com>
2024-11-12 22:50:32 -0300

    aula275 - adicionada a view vendas realizadas

---
+++ Controllers/SallesRecordsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers {
+    public class SallesRecordsController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult SampleSearch() {
+            return View();
+        }
+        public IActionResult GroupingSearch() {
+            return View();
+        }
+    }
+}
