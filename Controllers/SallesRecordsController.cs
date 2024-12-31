f3a9cced423b1d38874eb46780c06d94d96f9806
Pmigotto <pmigotto@hotmail.com>
2024-12-31 01:44:00 -0300

    aula 276 - immplementada a busca simples

--- Controllers/SallesRecordsController.cs
+++ Controllers/SallesRecordsController.cs
@@ -1,13 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace SalesWebMvc.Controllers {
     public class SallesRecordsController : Controller {
+        
+        private readonly SalesRecordService _salesRecordService;
+
+        public SallesRecordsController(SalesRecordService salesRecordService) {
+            _salesRecordService = salesRecordService;
+        }
         public IActionResult Index() {
             return View();
         }
 
-        public IActionResult SampleSearch() {
-            return View();
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate) {
+
+            if (!minDate.HasValue) {
+                minDate = new DateTime(DateTime.Now.Year,1,1);
+            }
+
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+
+            ViewData ["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData ["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+
+            return View(result);
         }
         public IActionResult GroupingSearch() {
             return View();
